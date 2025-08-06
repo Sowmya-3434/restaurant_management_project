63b509570f409e8db9ea036dfd52726e12901931
sowmyachanda17-oss <sowmyachanda17@gmail.com>
2025-08-06 13:34:02 +0530

    22019

--- home/views.py
+++ home/views.py
@@ -1,7 +1,1 @@
-import requests
-from django.shortcuts import render
-
-def homepage(requests):
-    response = requests.get("http://127.0.0.1:8000/products/menu-api")
-    menu = response.json().get('menu',[])
-    return render(request, 'home/homepage.html', {'menu': menu})
+hvjk
