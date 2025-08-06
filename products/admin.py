27cf63c17062a847bba17d7511d789c1a2a44d70
sowmyachanda17-oss <sowmyachanda17@gmail.com>
2025-08-06 14:14:02 +0530

    22021

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from .models import *
+from .models import Menu
 
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['item_name', 'item_price', 'created_at']
 
-# Custom Admins
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ['item_name','item_price','created_at']
-
-
-# Register your models here.
-admin.site.register(Item,ItemAdmin)
+admin.site.register(Menu, MenuAdmin)
