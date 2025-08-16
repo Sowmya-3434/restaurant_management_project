ba0d11146aff76028c6997a19a5e5cb410d85e4a
sowmyachanda17-oss <sowmyachanda17@gmail.com>
2025-08-16 11:33:02 +0530

    32715

--- home/views.py
+++ home/views.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from django.shortcuts import render
 
 def home(request):
+    context = {
+        "phone": settings.Restaurant_Phone
+    }
     return render(request, "home/index.html", {
         "restaurant_name": settings.restaurant_name
     })
