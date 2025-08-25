d88ce6bcc6ddd5b5b9591a244f7eef84ee1e5f10
sowmyachanda17-oss <sowmyachanda17@gmail.com>
2025-08-25 11:36:22 +0530

    37843

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('menu/', views.menu_view, name='menu'),
+    path('contact/', views.contact_view, name='contact'),
     
 ]
