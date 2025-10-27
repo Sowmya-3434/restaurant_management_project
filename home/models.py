60586dcfdc166f987d8562eff8903c12689efcc0
Sowmya-3434 <sowmya.chanda@aurora.edu.in>
2025-10-27 10:21:40 +0530

    42618

--- home/models.py
+++ home/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 
-class RestaurantLocation(models.Model):
-    address = models.CharField(max_length=255)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=10)
+class ContactSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
 
     def __str__(self):
-        return f"{self.address}, {self.city}, {self.state} - {self.zip_code}"
-   
+        return f"{self.name} - {self.email}"
