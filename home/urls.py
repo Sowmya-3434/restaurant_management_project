536936625376e159d2fe75b1ed42902af6c2bdb4
Sowmya-3434 <sowmya.chanda@aurora.edu.in>
2025-10-28 13:59:22 +0530

    56623

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('feedback/', views.feedback_view, name='feedback'),
+    path('menu-categories/', views.MenuCategoryListView.as_view(), name='menu-categories'),
     
 ]
