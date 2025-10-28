029bd1daf5dbb6276e2a6f14a9113b86ea894ae4
Sowmya-3434 <sowmya.chanda@aurora.edu.in>
2025-10-28 13:48:22 +0530

    56619

--- home/models.py
+++ home/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-class ContactSubmission(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique= True)
 
     def __str__(self):
-        return f"{self.name} - {self.email}"
+        return self.name
