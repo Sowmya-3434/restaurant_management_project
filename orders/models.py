d7f15594ec4279f15a41e59f7235181c51eeca8b
Sowmya-3434 <sowmya.chanda@aurora.edu.in>
2025-11-02 13:14:03 +0530

    56622

--- orders/models.py
+++ orders/models.py
@@ -1,21 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
-from products.models import Menu
 
-class Order(models.Model):
-    STATUS_CHOICES = [
-        ('PENDING', 'Pending'),
-        ('CONFIRMED', 'Confirmed'),
-        ('DELIVERED', 'Delivered'),
-        ('CANCELLED', 'Cancelled'),
-    ]
+class Coupon(models.Models):
+    code = models.CharField(max_length=50, unique=True)
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    valid_from = models.DateField()
+    valid_until = models.DateField()
 
-customer = models.ForeignKey(User, on_delete =models.CASCADE, related_name='orders')
-menu_item = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='orders')
-quantity = models.positiveIntegerField()
-total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
-created_at = models.DateTimeField(auto_now_add=True)
-
-def __str__(self):
-    return f"{self.customer.username} - {self.menu_item.name} - {self.status}"
+    def __str__(self):
+        return self.code
