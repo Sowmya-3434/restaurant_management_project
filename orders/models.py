122c61b01fe72fcf849c1a751c4e63247374f9b9
Sowmya-3434 <sowmya.chanda@aurora.edu.in>
2025-11-02 13:35:49 +0530

    56621

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class OrderStatus(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+    PENDING = 'pending'
+    PROCESSING = 'processing'
+    COMPLETED = 'completed'
+    CANCELLED = 'cancelled'
 
     def __str__(self):
         return self.name
