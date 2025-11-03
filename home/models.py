0ed6c82f5c4882f291f3b266f1be29522731e733
Sowmya-3434 <sowmya.chanda@aurora.edu.in>
2025-11-03 13:30:17 +0530

    22022

--- home/models.py
+++ home/models.py
@@ -1,10 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    description = models.TextField()
-    is_featured = models.BooleanField(default=False)
+class Coupon(models.Models):
+    code = models.CharField(max_length=50)
+    discount = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='coupons')
 
     def __str__(self):
-        return self.name
+        return self.code
