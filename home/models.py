e8a6d08e3aca0c2ac91f0388e054af25663ef4c1
Sowmya-3434 <sowmya.chanda@aurora.edu.in>
2025-11-03 12:45:13 +0530

    58036

--- home/models.py
+++ home/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-class Restaurant(models.Model):
+class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    has_delivery = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    is_featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
