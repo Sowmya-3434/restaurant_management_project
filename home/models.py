d2e70469bebbe3b78f3ef550303c8a8a329362a4
Sowmya-3434 <sowmya.chanda@aurora.edu.in>
2025-11-03 12:28:44 +0530

    58033

--- home/models.py
+++ home/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-class MenuCategory(models.Model):
-    name = models.CharField(max_length=100, unique= True)
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    has_delivery = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
