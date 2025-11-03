ffeb99afa98f7bde62a8f40e631386e34c635415
Sowmya-3434 <sowmya.chanda@aurora.edu.in>
2025-11-03 12:41:12 +0530

    58035

--- orders/views.py
+++ orders/views.py
@@ -1,23 +1,12 @@
 from rest_framework.views import APIView
-from rest_framework.response import response
-from rest_framework import status
-from .models import Coupon
+from rest_framework.response import Response
+from .models import Order
+from .serializers import OrderSerializer
 
-class CouponValidationView(APIView):
-    def post(self, request):
-        code = request.data.get('code')
-        if not code:
-            return Response({'error': 'coupon code is required.'}, status=status.HTTP_400_BAD_REQUEST)
-        try:
-            coupon = coupon.objects,get(code=code, is_active=True)
-        except Coupon.DoesNotExist:
-            return Response({'error': 'Invalid or inactive coupon'.}, status=status.HTTP_400_BAD_REQUEST)
-        today = date.today(
-            if not (coupon.valid_from <= coupon.valid_until):
-                return Response({'error': 'coupon is expired or not yet valid.'}, status=status.HTTP_400_BAD_REQUEST)
-            return Response({
-                'success': True,
-                'disount_percentage': float(coupon.discount_percentage),
-                'message': 'Coupon is valid.'
-            }, status=self.HTTP__OK)
-        
+class OrderHistoryView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        Orders = Order.objects.filter(user=request.user)
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
